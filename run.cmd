e5678d21804012a9cb0dd80828fb5bd70aefb786
pbrosset <patrick.brosset@amadeus.com>
2013-08-13 11:39:54 +0200

    removing run.cmd

--- run.cmd
+++
@@ -1,4 +0,0 @@
-REM eslint  -c .\conf\eslint.json --rulesdir .\lib\rules ..\..\Java\workspace\SEPL-0.17\webapps\ui\seplui\src\main\webapp\static\app\seplui\
-eslint  -c .\conf\eslint.json --rulesdir .\lib\rules D:\Java\workspace\ResourceManagement\webapps\ui\rm\src\main\webapp\static\app\rm
-REM eslint  -c .\conf\eslint.json --rulesdir .\lib\rules \\ncel743\jdevcc\UIPSITE\uipsite_v17.0_cr7247647\etv_uip\uip\modules-at
-REM eslint  -c .\conf\eslint.json --rulesdir .\lib\rules BundleDetailScript.js
